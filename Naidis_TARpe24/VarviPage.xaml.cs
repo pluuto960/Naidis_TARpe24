8b11b0fdb4c31acd8451c316c271014e76861908
Opilane TTHK <189347604+pluuto960@users.noreply.github.com>
2026-02-18 11:29:17 +0200

    ...

--- Naidis_TARpe24/VarviPage.xaml.cs
+++ Naidis_TARpe24/VarviPage.xaml.cs
@@ -2,8 +2,26 @@ namespace Naidis_TARpe24;
 
 public partial class VarviPage : ContentPage
 {
-	public VarviPage()
+    Label lbl;
+    Slider sl;
+    Slider sl2;
+    Slider sl3;
+    Stepper st;
+    AbsoluteLayout abs;
+    BoxView yleminekast;
+    BoxView yleminekast2;
+    BoxView yleminekast3;
+    public VarviPage()
 	{
-		InitializeComponent();
-	}
+        sl = new Slider
+        {
+            Minimum = 0,
+            Maximum = 100,
+            Value = 25,
+            MinimumTrackColor=Color.FromRgb(0,0,0),
+            MaximumTrackColor=Color.FromRgb(255,255,255),
+            ThumbColor=Color.FromRgb(155,155,155),
+        };
+
+    }
 }
