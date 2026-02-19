8bfe61e8896fadb007a5e22c52aefafb39f2ec7b
Opilane TTHK <189347604+pluuto960@users.noreply.github.com>
2026-02-19 09:54:43 +0200

    Uuendasin VarviPage, pole valmis

--- Naidis_TARpe24/VarviPage.xaml.cs
+++ Naidis_TARpe24/VarviPage.xaml.cs
@@ -1,4 +1,6 @@
 using Microsoft.Maui.Controls.Shapes;
+using Microsoft.Maui.Layouts;
+
 
 namespace Naidis_TARpe24;
 
@@ -18,23 +20,76 @@ public partial class VarviPage : ContentPage
         // =======Ülemised kastid=======
         yleminekast = new BoxView
         {
-            WidthRequest = 200,
-            HeightRequest = 200,            
+            WidthRequest = 50,
+            HeightRequest = 50,            
+            HorizontalOptions = LayoutOptions.Center
+        };
+        yleminekast2 = new BoxView
+        {
+            WidthRequest = 50,
+            HeightRequest = 50,
+            HorizontalOptions = LayoutOptions.Center
+        };
+        yleminekast3 = new BoxView
+        {
+            WidthRequest = 50,
+            HeightRequest = 50,
             HorizontalOptions = LayoutOptions.Center
         };
 
-
+        //=======Sliderid=======
         sl = new Slider
         {
             Minimum = 0,
-            Maximum = 100,
-            Value = 25,
-            MinimumTrackColor = Color.FromRgb(55, 55, 55),
-            MaximumTrackColor = Color.FromRgb(0, 0, 0),
+            Maximum = 255,
+            Value = 0,
+            WidthRequest = 300,
+            HorizontalOptions =LayoutOptions.Center,
+            MinimumTrackColor = Color.FromRgb(0, 0, 0),
+            MaximumTrackColor = Color.FromRgb(254, 0, 0),
+            ThumbColor = Color.FromRgb(155, 155, 155),
+        };
+        sl.ValueChanged += Sl_ValueChanged;
+        sl2 = new Slider
+        {
+            Minimum = 0,
+            Maximum = 255,
+            Value = 0,
+            WidthRequest=300,
+            HorizontalOptions = LayoutOptions.Center,
+            MinimumTrackColor = Color.FromRgb(0, 0, 0),
+            MaximumTrackColor = Color.FromRgb(0, 254, 0),
+            ThumbColor = Color.FromRgb(155, 155, 155),
+        };
+        sl.ValueChanged += Sl_ValueChanged;
+        sl3 = new Slider
+        {
+            Minimum = 0,
+            Maximum = 255,
+            Value = 0,
+            WidthRequest = 300,
+            HorizontalOptions = LayoutOptions.Center,
+            MinimumTrackColor = Color.FromRgb(0, 0, 0),
+            MaximumTrackColor = Color.FromRgb(0, 0, 254),
             ThumbColor = Color.FromRgb(155, 155, 155),
         };
         sl.ValueChanged += Sl_ValueChanged;
 
+
+        abs = new AbsoluteLayout { Children = { yleminekast, yleminekast2, yleminekast3, sl, sl2, sl3 } };
+        List<View> controls = new List<View> { yleminekast, yleminekast2, yleminekast3, sl, sl2, sl3 };
+        for (int i=0; i<controls.Count;i++)
+        {
+            double yKoht = 0.2 + i * 0.15;
+            AbsoluteLayout.SetLayoutBounds(controls[i], new Rect(0.5, yKoht, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+            AbsoluteLayout.SetLayoutFlags(controls[i], AbsoluteLayoutFlags.PositionProportional);
+        }
+        ScrollView scroll = new ScrollView
+        {
+            Content = abs
+        };
+
+        Content = scroll;
     }
     private void Sl_ValueChanged(object? sender, ValueChangedEventArgs e)
     {
