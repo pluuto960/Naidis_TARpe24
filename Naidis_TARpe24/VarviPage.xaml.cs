f28db5facfa39de1e880a63820624643a9a174a8
Opilane TTHK <189347604+pluuto960@users.noreply.github.com>
2026-02-25 08:59:14 +0200

    VarviPage done

--- Naidis_TARpe24/VarviPage.xaml.cs
+++ Naidis_TARpe24/VarviPage.xaml.cs
@@ -11,12 +11,25 @@ public partial class VarviPage : ContentPage
     Slider sl2;
     Slider sl3;
     Stepper st;
+    VerticalStackLayout vsl;
+    int R;
+    int G;
+    int B;
     AbsoluteLayout abs;
+    BoxView ColorBox;
     BoxView yleminekast;
     BoxView yleminekast2;
     BoxView yleminekast3;
     public VarviPage()
 	{
+        lbl = new Label
+        {
+            FontSize = 36,
+            FontFamily = "Corleone 400",
+            TextColor = Colors.Black,
+            Text = "RGB Mudel", 
+            HorizontalOptions = LayoutOptions.Center
+        };
         // =======Ülemised kastid=======
         yleminekast = new BoxView
         {
@@ -61,7 +74,7 @@ public partial class VarviPage : ContentPage
             MaximumTrackColor = Color.FromRgb(0, 254, 0),
             ThumbColor = Color.FromRgb(155, 155, 155),
         };
-        sl.ValueChanged += Sl_ValueChanged;
+        sl2.ValueChanged += Sl_ValueChanged;
         sl3 = new Slider
         {
             Minimum = 0,
@@ -73,26 +86,53 @@ public partial class VarviPage : ContentPage
             MaximumTrackColor = Color.FromRgb(0, 0, 254),
             ThumbColor = Color.FromRgb(155, 155, 155),
         };
-        sl.ValueChanged += Sl_ValueChanged;
-
-
-        abs = new AbsoluteLayout { Children = { yleminekast, yleminekast2, yleminekast3, sl, sl2, sl3 } };
-        List<View> controls = new List<View> { yleminekast, yleminekast2, yleminekast3, sl, sl2, sl3 };
-        for (int i=0; i<controls.Count;i++)
+        sl3.ValueChanged += Sl_ValueChanged;
+        ColorBox = new BoxView
         {
-            double yKoht = 0.2 + i * 0.15;
-            AbsoluteLayout.SetLayoutBounds(controls[i], new Rect(0.5, yKoht, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
-            AbsoluteLayout.SetLayoutFlags(controls[i], AbsoluteLayoutFlags.PositionProportional);
-        }
-        ScrollView scroll = new ScrollView
+            Color = Color.FromRgb(R, G, B),
+            WidthRequest = 200,
+            HeightRequest = 200,
+            HorizontalOptions = LayoutOptions.Center,
+            BackgroundColor = Color.FromRgba(0, 0, 0, 0),
+            CornerRadius = 15,
+        };
+        vsl = new VerticalStackLayout
         {
-            Content = abs
+            Padding = 20,
+            Spacing = 15,
+            Children = { lbl, yleminekast, sl, yleminekast2, sl2, yleminekast3, sl3, ColorBox },
+            HorizontalOptions = LayoutOptions.Center
         };
 
-        Content = scroll;
+        //abs = new AbsoluteLayout { Children = { yleminekast, sl, yleminekast2, sl2, yleminekast3, sl3, ColorBox } };
+        //List<View> controls = new List<View> { yleminekast, sl, yleminekast2, sl2, yleminekast3, sl3, ColorBox };
+        //for (int i=0; i<controls.Count;i++)
+        //{
+        //    double yKoht = 0.2 + i * 0.15;
+        //    AbsoluteLayout.SetLayoutBounds(controls[i], new Rect(0.5, yKoht, AbsoluteLayout.AutoSize, AbsoluteLayout.AutoSize));
+        //    AbsoluteLayout.SetLayoutFlags(controls[i], AbsoluteLayoutFlags.PositionProportional);
+        //}
+        //ScrollView scroll = new ScrollView
+        //{
+        //    Content = abs
+        //};
+
+        Content = vsl;
     }
     private void Sl_ValueChanged(object? sender, ValueChangedEventArgs e)
     {
-        
+        R = (int)sl.Value;
+        G = (int)sl2.Value;
+        B = (int)sl3.Value;
+        sl.MinimumTrackColor = Color.FromRgb(R, 0, 0);
+        sl.MaximumTrackColor = Color.FromRgb((255 - R), 0, 0);
+        sl2.MinimumTrackColor = Color.FromRgb(0, G, 0);
+        sl2.MaximumTrackColor = Color.FromRgb(0, (255 - G), 0);
+        sl3.MinimumTrackColor = Color.FromRgb(0, 0, B);
+        sl3.MaximumTrackColor = Color.FromRgb(0, 0, (255 - B));
+        yleminekast.BackgroundColor = Color.FromRgb(R, 0, 0);
+        yleminekast2.BackgroundColor = Color.FromRgb(0, G, 0);
+        yleminekast3.BackgroundColor = Color.FromRgb(0, 0, B);
+        ColorBox.Color = Color.FromRgb(R, G, B);
     }
 }
